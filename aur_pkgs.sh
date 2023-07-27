465eb32d225ed53f5127aa06b43723ccce2a6fe5
Your Name <you@example.com>
2023-07-27 15:11:51 +0000

    add

--- aur_pkgs.sh
+++ aur_pkgs.sh
@@ -3,6 +3,7 @@ if [[ ! -d local/repo/ ]]; then
   mkdir -p local/repo/
 fi
 pkgs="
+waybar-hyprland-git
 brillo
 cli-visualizer
 python-spotdl
@@ -12,10 +13,12 @@ rofi-lbonn-wayland-git
 bluetuith-bin
 transg-tui-git
 grimblast-git
-grimblast-git
+paru
 bibata-cursor-theme-bin
 nerd-fonts-complete-mono-glyphs
 rofi-games 
+python-syncedlyrics
+python-jaconv
 "
 for k in $pkgs
 do
@@ -26,4 +29,7 @@ do
    cd ..
    rm -rf $k
 done
+curl -LO https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst
+curl -LO https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst
+mv *zst local/repo/
 repo-add local/repo/custom.db.tar.gz local/repo/*.zst
